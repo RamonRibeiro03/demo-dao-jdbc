1dce9fca8b27ffa654be54b51babadcd729c5b1e
Ramon Ribeiro <ramon.procisa@gmail.com>
2023-07-13 00:31:31 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,13 @@ public class Program {
         sellerDao.insert(newseller);
         System.out.println("Inserted! New id = " + newseller.getId());
 
+        System.out.println("\n === TEST 5 : seller update =====");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
+
 
 
 
